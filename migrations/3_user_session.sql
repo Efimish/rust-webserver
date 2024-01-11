ec51a083594b3b230ab0d7287cbcd072666385a7
Efimish <efim.ishenin@gmail.com>
2024-01-12 01:18:47 +0700

    merged user and user_profile models

--- migrations/3_user_session.sql
+++ migrations/3_user_session.sql
@@ -4,7 +4,8 @@ create table user_session
     session_id uuid not null,
     user_ip text not null,
     user_agent text not null,
-    user_location text not null,
+    user_country text not null,
+    user_city text not null,
     last_active timestamptz not null default now(),
     unique(session_id)
 );
