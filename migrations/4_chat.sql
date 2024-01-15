27d93fad722d9fde304a58a96acfe8a7e28bdde2
Efimish <efim.ishenin@gmail.com>
2024-01-15 16:26:08 +0700

    change chat model

--- migrations/4_chat.sql
+++ migrations/4_chat.sql
@@ -1,7 +1,5 @@
-create type chat_type as enum ('saved', 'private', 'group');
-
 create table chat
 (
     chat_id uuid primary key default uuid_generate_v4(),
-    chat_type chat_type
+    chat_name text not null
 );
