b14b36247dba1f9006e351cc4e0eddf31eacd069
Efimish <efim.ishenin@gmail.com>
2024-01-27 00:06:55 +0700

    change uploads schema

--- migrations/10_upload.sql
+++ migrations/10_upload.sql
@@ -2,8 +2,9 @@ create table upload
 (
     upload_id uuid primary key default uuid_generate_v4(),
     file_name text not null,
-    file_path text not null,
+    extension text not null,
     content_type text not null,
+    folder text not null,
     size bigint not null,
     created_at timestamptz not null default now()
 );
